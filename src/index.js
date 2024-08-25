fbc5fd44eeb3035836b6f539c5a0b758e492205c
Thinkshareyun <47290639+Thinkshareyun@users.noreply.github.com>
2024-08-25 14:26:01 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.thinkshareyun.top": dockerHub,
+  "quay.thinkshareyun.top": "https://quay.io",
+  "gcr.thinkshareyun.top": "https://gcr.io",
+  "k8s-gcr.thinkshareyun.top": "https://k8s.gcr.io",
+  "k8s.thinkshareyun.top": "https://registry.k8s.io",
+  "ghcr.thinkshareyun.top": "https://ghcr.io",
+  "cloudsmith.thinkshareyun.top": "https://docker.cloudsmith.io",
+  "ecr.thinkshareyun.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.thinkshareyun.top": dockerHub,
 };
 
 function routeByHosts(host) {
